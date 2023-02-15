ff710f340321d8137a72bc2c106ba757767f7277
Renato <boniex01@gmail.com>
2023-02-15 15:13:17 -0300

    novos

---
+++ Estruturas_de_controle/exer/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 5;
+$b = 10;
+
+if ($a > $b) {
+    echo "a is greater than b";
+} elseif ($a == $b) {
+    echo "a is equal to b";
+} else {
+    echo "a is less than b";
+}
---
+++ Estruturas_de_controle/index.php
@@ -0,0 +1,27 @@
+<?php
+    //condição verdadeira
+    if(5>2){
+        echo "Deu certo! entro no if 1 <br>";
+    }
+    //condição falsa
+
+    if(5<2){
+        echo "Deu certo! entro no if 2<br>";
+    }
+
+    //ultilizar operadores lógicos
+
+    if(5>2 && 5<10){
+        echo "Deu certo! entro no if 3<br>";
+    }
+
+    //variavel
+    $a = 5;
+    $b = 10;
+
+    $c = "deu certo entro no if 3<br>";
+
+
+    if($a<=$b){
+        echo $c;
+    }
